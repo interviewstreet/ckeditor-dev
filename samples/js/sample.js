1242d00afc0ba89796aa587d81107dd5b45587d5
Siva Priya <sivapriya@hackerrank.com>
2022-09-28 18:18:35 +0530

    DropDown element change

--- samples/js/sample.js
+++ samples/js/sample.js
@@ -12,13 +12,89 @@ if ( CKEDITOR.env.ie && CKEDITOR.env.version < 9 )
 // unless user specified own height.
 CKEDITOR.config.height = 150;
 CKEDITOR.config.width = 'auto';
-
+function getConfig(){
+  // Config used in hackerrank
+  return {
+    toolbar : [
+      ["Bold", "Italic", "Underline", "Strike", "Subscript", "Superscript", "-", "NumberedList", "BulletedList", "-", "Indent", "Outdent", "-", "Link", "Unlink"],
+      ["Styles","Format", "Font", "FontSize", ],
+      {
+        name: 'clipboard',
+        items: ['Cut','Copy','-','PasteMenu',
+        'Undo','Redo', 'Image', 'Table' ]
+      },
+      ["Source", "CodeSnippet"],
+      ["Templates"],
+      ["MoreStyles", "TextColor"]
+    ],
+    toolbarCanCollapse: false,
+    autoGrow_onStartup: true,
+    extraAllowedContent: 'iframe[*];*{*}',
+    allowedContent: true,
+    removePlugins: "elementspath,liststyle,link",
+    removeDialogTabs: 'link:advanced;link:target;link:upload',
+    disableNativeSpellChecker: false,
+    bodyClass: 'hrx-version challenge-text ck_table-wrap',
+    htmlEncodeOutput: true,
+    tabSpaces: 4,
+    language:'en',
+    defaultLanguage:'en',
+    extraPlugins : 'widget,dialog,codesnippet,image2,customlink,templates,autogrow,latex,dropdownmenumanager,menu,menubutton',
+    dropdownmenumanager: {
+      'PasteMenu': {
+        items:[{
+          name: 'Paste',
+          command: 'paste'
+         }, {
+           name: 'PasteText',
+           label: 'Paste as text',
+           command: 'pastetext'
+         }, {
+           name: 'PasteFromWord',
+           label:  'Paste from Word',
+           command: 'pastefromword'
+         }],
+        label: {
+          text: 'Paste',
+          width: 100,
+          visible:true //default value
+        },
+        iconPath: 'paste', //You can use global icons or absolute path to the icon
+        toolbar: 'clipboard', // to specify toolbar group for button  // NOT-needed
+      },
+      'MoreStyles': {
+        items:[
+          {
+            name: 'TextColor',
+            label: 'Text color',
+            onClick: function(){
+              document.querySelector('.cke_button__textcolor').click(); // Make sure the query selector selects the current element
+            },
+            command: 'formulaeCmd'
+         }, {
+           name: 'Latex',
+           label: 'Latex',
+           command: 'formulaeCmd',
+           icon: 'latex'
+         }],
+        label: {
+          text: 'Additional Styles',
+          width: 45,
+          visible:true //default value
+        },
+        iconPath: 'kebab', //You can use global icons or absolute path to the icon
+        toolbar: 'more'
+      },
+  }
+  }
+}
+let editor;
 var initSample = ( function() {
 	var wysiwygareaAvailable = isWysiwygareaAvailable(),
 		isBBCodeBuiltIn = !!CKEDITOR.plugins.get( 'bbcode' );
 
 	return function() {
-		var editorElement = CKEDITOR.document.getById( 'editor' );
+		var editorElement = CKEDITOR.document.getById( 'editor');
 
 		// :(((
 		if ( isBBCodeBuiltIn ) {
@@ -30,15 +106,42 @@ var initSample = ( function() {
 
 		// Depending on the wysiwygarea plugin availability initialize classic or inline editor.
 		if ( wysiwygareaAvailable ) {
-			CKEDITOR.replace( 'editor' );
+      const config = getConfig();
+			editor = CKEDITOR.replace( 'editor', config);
 		} else {
 			editorElement.setAttribute( 'contenteditable', 'true' );
-			CKEDITOR.inline( 'editor' );
+			editor = CKEDITOR.inline( 'editor', getConfig() );
 
 			// TODO we can consider displaying some info box that
 			// without wysiwygarea the classic editor may not work.
 		}
+    function getOnChange(name){
+      return (evt) => {
+        console.log("EVENT", evt);
+        console.log("NAME", name);
+      }
+    }
+
+
+  function handleBeforeCommandExec(event){
+    var commandName = event.data.name;
+    // Don't intercept Paste Event here since CKEditor paste seems to be not working https://github.com/ckeditor/ckeditor4/issues/469
+    console.log("You have pressed the " + commandName);
+  }
+  
+    editor.on('beforeCommandExec', handleBeforeCommandExec);
+    editor.on('paste', getOnChange('paste'))
+    
+    let oldStyle = editor.applyStyle;
+
+    editor.applyStyle = function(...args){
+      const styleDefinition = args[0].getDefinition() || {};
+      const styles = styleDefinition.styles;
+      console.log({ styles });
+      oldStyle.call(this, ...args);
+    }
 	};
+
 
 	function isWysiwygareaAvailable() {
 		// If in development mode, then the wysiwygarea must be available.
